f9e35e6c50afae644fbf9b086a6b6be75c78dc99
Glotov Vladimir <glotovv@ae.com>
2019-12-16 17:06:33 +0400

    comments fixes

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --user virtualenv
 python3 -m testenv env
 pip install -r install/requirements.txt
-python -m pytest models/BookModel_test.py
+python -m pytest models/BookModel_test.py app_test.py
 python3 app.py
