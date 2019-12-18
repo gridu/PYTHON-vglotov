184423165b3352fd39fc8a0ed19d891860fb88f6
Glotov Vladimir <glotovv@ae.com>
2019-12-18 18:28:05 +0400

    comments fix

--- app_test.py
+++ app_test.py
@@ -1,33 +1,33 @@
 from app import valid_book_object, valid_book_object_to_rename
+import unittest
 
 
-def test_valid_book_object_to_add_recognized():
-    book = {
-        'creation_date': '1998-10-11',
-        'title': 'Pomi Dore',
-        'type': 'Satire'
-    }
-    assert valid_book_object(book)
+class BookValidityChecktest(unittest.TestCase):
 
+    def test_valid_book_object_to_add_recognized(self):
+        book = {
+            'creation_date': '1998-10-11',
+            'title': 'Pomi Dore',
+            'type': 'Satire'
+        }
+        assert valid_book_object(book)
 
-def test_invalid_book_object_to_add_recognized():
-    book = {
-        'creation_date': '1998-10-11',
-        'type': 'Pomi Dore'
-    }
-    assert not valid_book_object(book)
+    def test_invalid_book_object_to_add_recognized(self):
+        book = {
+            'creation_date': '1998-10-11',
+            'type': 'Pomi Dore'
+        }
+        assert not valid_book_object(book)
 
+    def test_valid_book_object_to_rename_recognized(self):
+        book = {
+            'id': 2,
+            'title': 'Pomi Dore'
+        }
+        assert valid_book_object_to_rename(book)
 
-def test_valid_book_object_to_rename_recognized():
-    book = {
-        'id': 2,
-        'title': 'Pomi Dore'
-    }
-    assert valid_book_object_to_rename(book)
-
-
-def test_invalid_book_object_to_rename_recognized():
-    book = {
-        'title': 'Pomi Dore'
-    }
-    assert not valid_book_object_to_rename(book)
+    def test_invalid_book_object_to_rename_recognized(self):
+        book = {
+            'title': 'Pomi Dore'
+        }
+        assert not valid_book_object_to_rename(book)
