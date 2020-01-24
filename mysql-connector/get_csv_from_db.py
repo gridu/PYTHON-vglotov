c118db72f16e986f14fd85a92c6b3dda72ea57cb
Glotov Vladimir <glotovv@ae.com>
2020-01-24 20:51:19 +0400

    extracted methods

--- mysql-connector/get_csv_from_db.py
+++ mysql-connector/get_csv_from_db.py
@@ -3,15 +3,16 @@ import pandas as pd
 import logging
 from mysql.connector import errorcode
 
-DB_NAME = 'booksDB'
+DB_NAME = 'booksDataBase'
+TABLE_NAME = 'test_data1'
 
-TABLES = {'test_data': (
-    "CREATE TABLE test_data( date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-    " title VARCHAR(500) NOT NULL,"
-    " author VARCHAR(500) NOT NULL)")}
+TABLES = {TABLE_NAME: (
+        "CREATE TABLE " + TABLE_NAME + " ( date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+                                       " title VARCHAR(500) NOT NULL,"
+                                       " author VARCHAR(500) NOT NULL)")}
 
-ADD_DATA = "INSERT INTO test_data (title, author) VALUES ('title', 'author')"
-SELECT_ALL = "SELECT * FROM test_data"
+ADD_DATA_QUERY = "INSERT INTO {} (title, author) VALUES ('title', 'author')".format(TABLE_NAME)
+SELECT_ALL_QUERY = "SELECT * FROM {}".format(TABLE_NAME)
 
 LOGGER = logging.getLogger()
 logging.basicConfig(
@@ -24,9 +25,36 @@ def create_database(_cursor):
     try:
         _cursor.execute(
             "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-    except mysql.connector.Error as err:
-        LOGGER.info("Failed creating database: {}".format(err))
+    except mysql.connector.Error as _err:
+        LOGGER.info("Failed creating database: {}".format(_err))
         exit(1)
+
+
+def create_tables(_TABLES):
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+        try:
+            LOGGER.info("Creating table '{}' ".format(table_name))
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                LOGGER.info("Already exists")
+            else:
+                LOGGER.info(err.msg)
+        else:
+            LOGGER.info("OK")
+
+
+def extract_table_data_to_csv(_cursor, path_to_csv, _DB_NAME, _table_name):
+    columns_query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = " \
+                    "'{}'".format(_DB_NAME, _table_name)
+    _cursor.execute(SELECT_ALL_QUERY)
+    selected_data = _cursor.fetchall()
+    _cursor.execute(columns_query)
+    _columns = cursor.fetchall()
+    _columns = [column[0] for column in _columns]
+    table_data = pd.DataFrame(selected_data, columns=_columns)
+    table_data.to_csv(path_to_csv, index=True)
 
 
 try:
@@ -46,38 +74,14 @@ try:
             LOGGER.info(err)
             exit(1)
 
-    for table_name in TABLES:
-        table_description = TABLES[table_name]
-        try:
-            LOGGER.info("Creating table '{}' ".format(table_name))
-            cursor.execute(table_description)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                LOGGER.info("Already exists")
-            else:
-                LOGGER.info(err.msg)
-        else:
-            LOGGER.info("OK")
-
-    columns_query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = " \
-                    "'test_data'".format(DB_NAME)
-
-    cursor.execute(ADD_DATA)
+    create_tables(TABLES)
+    cursor.execute(ADD_DATA_QUERY)
     cnx.commit()
-    cursor.execute(SELECT_ALL)
-    selected_data = cursor.fetchall()
-    cursor.execute(columns_query)
-    columns = cursor.fetchall()
-    columns = [column[0] for column in columns]
-    table_data = pd.DataFrame(selected_data, columns=columns)
-    table_data.to_csv('mysql-connector/testCSV.csv', index=True)
-
+    extract_table_data_to_csv(cursor, 'mysql-connector/testCSV.csv', DB_NAME, TABLE_NAME)
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         LOGGER.info("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        LOGGER.info("Database does not exist")
     else:
         LOGGER.info(err)
 else:
